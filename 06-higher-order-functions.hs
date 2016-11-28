ef4d1a880dfd6df5dd9a748b16e674757dbbfc0a
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2016-11-28 22:02:53 +0000

    some corrections to my exercise solutions - following discussion in the book club

--- 06-higher-order-functions.hs
+++ 06-higher-order-functions.hs
@@ -1,34 +1,34 @@
--- Sum the numbers between two inclusive values recursively, assuming a < b when the function is first called
+-- Sum the numbers between two inclusive values , assuming a < b when the function is first called
 -- Example: sumInts 0 1 = 1
 --          sumInts 1 3 = 6
 sumInts :: Int -> Int -> Int
-sumInts a b = sum [a..b]
+sumInts a b = sum [a .. b]
 
 -- Define a square function
 sq :: Int -> Int
-sq x = x*x
+sq x = x * x
 
 -- Sum the squares between two numbers. This function should be similar to the sumInts function
 sumSquares :: Int -> Int -> Int
-sumSquares a b = sum $ map sq [a..b]
+sumSquares a b = sum $ map sq [a .. b]
 
 -- Define a higher order sum function which accepts an (Int -> Int) function to apply to all integers between two values.
 -- Again this should look similar to the sumInts and sumSquares functions
 higherOrderSum :: (Int -> Int) -> Int -> Int -> Int
-higherOrderSum intApplication a b = sum $ map intApplication [a..b]
+higherOrderSum intApplication a b = sum  $ map intApplication [a..b]
 
 -- Define the square sum in terms of higherOrderSum
 hoSumSquares :: Int -> Int -> Int
-hoSumSquares a b = higherOrderSum sq a b
+hoSumSquares = higherOrderSum sq
 
 -- Define the sum between two values in terms of higherOrderSum
 -- Note there is no parameter on the function definition
 -- Try to use a lambda if possible
 hoSumInts :: Int -> Int -> Int
-hoSumInts = (\a b -> higherOrderSum id a b) where id = (\x -> x)
+hoSumInts = higherOrderSum id
 
 -- Create a new higher order method which generalises over the function provided by sumInts (That is, parameterize (+) :: Int -> Int -> Int) between a and b
--- This will give the ability to perform utilities such as the product of all squares (or any other Int -> Int function) between a and b
+-- This will give the ability to perform utilities such as the prodcut of all squares (or any other Int -> Int function) between a and b
 -- You will also need to generalise the base case
 -- You can also define the function signature yourself, which leaves you free to define the parameters and their order
 -- To be clear, your function will need to handle:
@@ -37,9 +37,11 @@ hoSumInts = (\a b -> higherOrderSum id a b) where id = (\x -> x)
 --  - A function to apply to each value, op :: Int -> Int
 --  - A function to apply between each value, f :: Int -> Int -> Int
 --  - A value to return in the base case when a > b, z :: Int
-higherOrderSequenceApplication :: (Int -> Int) -> (Int -> Int -> Int) -> Int -> Int -> Int -> Int
-higherOrderSequenceApplication op f a b z | a > b = z | otherwise = foldl f z $ map op [a..b]
+higherOrderSequenceApplication :: (Num a, Ord a, Enum a) => (a -> a -> a) -> (a -> a) -> a -> a -> a -> a
+higherOrderSequenceApplication f op z a b
+    | a > b = z
+    | otherwise = foldl f z $ map op [a .. b]
 
--- Define a factorial method using the higherOrderSequenceApplication
-hoFactorial :: Int -> Int
-hoFactorial n = higherOrderSequenceApplication (+0) (*) 1 n 1
+-- Define a factorial method using the higherOrderSequenceAppliction
+hoFactorial :: Integer -> Integer
+hoFactorial = higherOrderSequenceApplication (*) id 1 1
