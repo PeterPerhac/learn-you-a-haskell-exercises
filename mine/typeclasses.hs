5176b73e337655ae5c9831cf87dcce8827cd7fd5
Peter Perhac <perhac.peter@gmail.com>
2016-12-05 22:41:46 +0000

    adding my tinkering with typeclasses

---
+++ mine/typeclasses.hs
@@ -0,0 +1,37 @@
+--
+-- new YesNo typeclass
+-- 
+
+class YesNo a where
+    yesno :: a -> Bool
+
+
+-- 
+-- and some types of the YesNo class
+--
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo Bool where
+    yesno = id
+
+instance YesNo (Maybe a) where  
+    yesno (Just _) = True  
+    yesno Nothing = False  
+
+
+--
+-- additional functions
+--
+
+yesnoIf :: (YesNo y) => y -> a -> a -> a  
+yesnoIf yesnoVal yesResult noResult = if yesno yesnoVal then yesResult else noResult  
+
+data Color = Red | Green | Blue deriving (Enum, Ord, Eq, Bounded, YesNo)
+
